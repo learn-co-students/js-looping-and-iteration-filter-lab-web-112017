b636ca3ad96a316964c3c728c330740c17943d76
KellyStamps <kellydsample@gmail.com>
2018-01-08 15:10:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file
+// drivers = ['hi', 'low', 'hi']
+// string = 'hi'
+
+function findMatching (drivers, string) {
+  final = [];
+  for (const name of drivers) {
+    if (name.toLowerCase() === string.toLowerCase()) {
+      final.push(name);
+    }
+  }
+  return final;
+}
+
+function fuzzyMatch (drivers, string) {
+  final = []
+  for (const name of drivers) {
+    if (name[0].toLowerCase() === string[0].toLowerCase()) {
+      final.push(name)
+    }
+  }
+  return final;
+}
+
+function matchName (drivers, string) {
+  final = [];
+  for (const person of drivers) {
+    if (person.name === string) {
+      final.push(person);
+    }
+  }
+  return final;
+}
