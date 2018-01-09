dd552ac922c451fffac3592a16ada9de0e99213c
L-A-Davis <lindsay.amanda.davis@gmail.com>
2018-01-08 21:01:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, name) {
+  return array.filter(function (item) {
+    return item.toLowerCase() === name.toLowerCase()
+  } )
+}
+
+function fuzzyMatch(array, part) {
+  let part_length = part.length
+  return array.filter(function (item) {
+    return item.slice(0, part_length) === part
+  })
+}
+
+function matchName(array, arg) {
+  return array.filter(function (item) {
+  return item.name === arg  
+  })
+}
