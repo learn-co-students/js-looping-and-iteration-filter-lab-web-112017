329964321afd727a6f06dd7d6a87140298f2f137
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-01-09 13:46:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+
+function findMatching (drivers, string) {
+  return drivers.filter(function(value) {
+    return value.toLowerCase() === string.toLowerCase();
+  })
+}
+
+function fuzzyMatch (drivers, string) {
+  let numberOfLetters = string.length;
+  return drivers.filter(function(value) {
+    return value.slice(0, numberOfLetters) === string;
+  });
+}
+
+
+function matchName(drivers, string) {
+  return drivers.filter(function(value) {
+    return value.name.toLowerCase() === string.toLowerCase();
+  });
+}
