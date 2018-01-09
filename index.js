04c686286adb042d945c9251d8d717f76d241399
Flatiron School <leslie.hsu@flatironschool.com>
2018-01-09 12:44:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file
+
+// function filter (collection, cb) {
+//   const newCollection = [];
+//
+//   for (const user of collection) {
+//     if (cb(user)) {
+//       newCollection.push(user);
+//     }
+//   }
+//
+//   return newCollection;
+// }
+
+
+
+function findMatching (drivers, nameToMatch) {
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === nameToMatch.toLowerCase();
+  });
+}
+
+
+
+function fuzzyMatch (drivers, chars) {
+  return drivers.filter(function(driverName){
+    return driverName.slice(0, chars.length) === chars
+  })
+}
+
+
+function matchName (drivers, nameToMatch) {
+  return drivers.filter(function (driverObj) {
+    return driverObj.name.toLowerCase() === nameToMatch.toLowerCase();
+  });
+}
