687df4a8f79394091980a3e0c8aa072c458741c1
Marvin Clerge <clergemarvin@gmail.com>
2018-01-09 13:08:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file
+
+
+function findMatching(collection, name){
+  newCollection = [];
+  for (const driver of collection) {
+    if (driver.toLowerCase() === name.toLowerCase()) {
+      console.log(`${driver} was added to collection.`);
+      newCollection.push(driver);
+    }
+  }
+  return newCollection;
+}
+
+function fuzzyMatch(collection, name, callback) {
+  newCollection = [];
+  for (callback(collection, name) ) {
+      console.log(`${driver} was added to collection.`);
+      newCollection.push(driver);
+    }
+  }
+  return newCollection;
+}
+
+function matchName(collection, name) {
+  newCollection = [];
+  for (const driver of collection) {
+    if (name.toLowerCase() === driver.name.toLowerCase()) {
+      console.log('HERE: ' + driver.name)
+      newCollection.push(driver);
+    }
+  }
+  return newCollection;
+}
